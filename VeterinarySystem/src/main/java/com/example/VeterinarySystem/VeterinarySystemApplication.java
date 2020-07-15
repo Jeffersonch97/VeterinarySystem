4471b94432906a0834405c3cc273fa833d163cf4
Janter <Janter@Janter>
2020-07-15 15:37:54 -0500

    Test01

--- VeterinarySystem/src/main/java/com/example/VeterinarySystem/VeterinarySystemApplication.java
+++ VeterinarySystem/src/main/java/com/example/VeterinarySystem/VeterinarySystemApplication.java
@@ -8,7 +8,7 @@ public class VeterinarySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeterinarySystemApplication.class, args);
-		System.out.println("");
+		System.out.println("Prueba Numero 1");
 	}
 
 }
