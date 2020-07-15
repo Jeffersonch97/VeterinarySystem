4c712ac6c674f43cefe0ae0d4b8beacd7fcb50ac
DENNIS <DENNIS@DESKTOP-8FM59F6>
2020-07-15 15:29:49 -0500

    comentario prueba

--- VeterinarySystem/src/main/java/com/example/VeterinarySystem/VeterinarySystemApplication.java
+++ VeterinarySystem/src/main/java/com/example/VeterinarySystem/VeterinarySystemApplication.java
@@ -8,6 +8,7 @@ public class VeterinarySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeterinarySystemApplication.class, args);
+		System.out.println("");
 	}
 
 }
