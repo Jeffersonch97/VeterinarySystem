b5a4651a0633019536299f7a5581a7b579488c03
Janter <Janter@Janter>
2020-07-21 19:27:18 -0500

    Persona01

--- VeterinarySystem/src/main/java/com/example/VeterinarySystem/entities/Persona.java
+++ VeterinarySystem/src/main/java/com/example/VeterinarySystem/entities/Persona.java
@@ -9,17 +9,73 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Persona")
+@Table(name = "Persona")
 
 public class Persona {
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-private String Cedula;
-private String Nombre;
-private String Apellido;
-private String Genero;
-private String Usuario;
-private String Contrasenia;
-private String String;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String Cedula;
+	private String Nombre;
+	private String Apellido;
+	private String Genero;
+	private String Usuario;
+	private String Contrasena;
+	private String String;
+
+	public String getCedula() {
+		return Cedula;
+	}
+
+	public void setCedula(String cedula) {
+		Cedula = cedula;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+
+	public String getGenero() {
+		return Genero;
+	}
+
+	public void setGenero(String genero) {
+		Genero = genero;
+	}
+
+	public String getUsuario() {
+		return Usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		Usuario = usuario;
+	}
+
+	public String getContrasena() {
+		return Contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		Contrasena = contrasena;
+	}
+
+	public String getString() {
+		return String;
+	}
+
+	public void setString(String string) {
+		String = string;
+	}
 
 }
