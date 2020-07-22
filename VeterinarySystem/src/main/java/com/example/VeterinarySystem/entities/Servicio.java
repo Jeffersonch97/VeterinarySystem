32b24004840804cff2ee6a7c29d1c3a7081e40a3
DENNIS <DENNIS@DESKTOP-8FM59F6>
2020-07-22 16:03:17 -0500

    zd

--- VeterinarySystem/src/main/java/com/example/VeterinarySystem/entities/Servicio.java
+++ VeterinarySystem/src/main/java/com/example/VeterinarySystem/entities/Servicio.java
@@ -1,6 +1,43 @@
 package com.example.VeterinarySystem.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Servicio")
+
 public class Servicio {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String tipo;
+	private String descuento;
+	private String precio;
+	
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(String descuento) {
+		this.descuento = descuento;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	
+	
+	
 	
 
 }
