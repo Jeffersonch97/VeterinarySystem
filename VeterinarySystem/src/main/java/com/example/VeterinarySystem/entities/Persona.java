92318170af95a32d5eab1d7f6705e5d5b0425bff
Janter <Janter@Janter>
2020-07-21 19:00:48 -0500

    Persona

--- VeterinarySystem/src/main/java/com/example/VeterinarySystem/entities/Persona.java
+++ VeterinarySystem/src/main/java/com/example/VeterinarySystem/entities/Persona.java
@@ -1,5 +1,16 @@
 package com.example.VeterinarySystem.entities;
 
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="Persona")
+
 public class Persona {
 
 }
