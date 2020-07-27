c93fa5a2cc673b9e071533e61c2047fae5610027
Janter <Janter@Janter>
2020-07-27 12:09:57 -0500

    Beta0.1

--- VeterinarySystem/src/main/java/com/example/VeterinarySystem/contoller/VeterinaryController.java
+++ VeterinarySystem/src/main/java/com/example/VeterinarySystem/contoller/VeterinaryController.java
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.VeterinarySystem.entities.*;
 import com.example.VeterinarySystem.repository.*;
 import com.example.VeterinarySystem.service.PictureService;
+import com.sun.xml.bind.v2.runtime.IllegalAnnotationsException;
 
 @Controller
 @RequestMapping("/recetas")
@@ -31,4 +32,96 @@ public class VeterinaryController {
 	{
 		return "index";
 	}
+	
+	@GetMapping("/signup")
+	public String showSignUpForm(Recetas receta)
+	{
+		return "add_recipe";
+	}
+	
+	@GetMapping("/list")
+	public String showRecipes(Model model)
+	{
+		model.addAttribute("recipes", repo.findAll());
+		return "list_recipes";
+	}
+	
+	@RequestMapping("/login")
+	public String showLogin()
+	{
+		return "login";
+	}
+	
+	@GetMapping("/sc")
+	public String showRecipes()
+	{
+		return "soundcloud.html";
+	}
+	
+	@PreAuthorize("hasAuthority('Admin')")
+	@RequestMapping
+	public String showPrivate(Model model)
+	{
+		model.addAttribute("recipes",repo.findAll());
+		return "list_recipes";
+	}
+	
+	@PreAuthorize("hasAuthority('admin')")
+	@PostMapping("/add")
+	public String addRecipe(Receta receta,BindingResult result, Model model, @RequestParam("file") MultipartFile file)
+	{
+		if(result.hasErrors())
+		{
+			return "add_recipe";
+		}
+		UUID idPic = UUID.randomUUID();
+		picService.uploadPicture(file, idPic);
+		receta.setFoto(idPic);
+		repo.save(receta);
+		return "redirect:list";
+	}
+	
+	@PreAuthorize("hasAuthority('admin')")
+	@GetMapping("/edit/{id}")
+	public String showUpdateForm(@PathVariable("id") Long id,Model model)
+	{
+		Receta receta = repo.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid recipe Id:")+id));
+		model.addAttribute("recipe",receta);
+		return "update_recipe";
+	}
+	
+	@PreAuthorize("hasAuthority('admin')")
+	@PostMapping("/update/{id}")
+	public String updateRecipe(@PathVariable("id") Long id, Receta receta, BindingResult result, Model model, @RequestParam("file") MultipartFile file)
+	{
+		if(result.hasErrors())
+		{
+			receta.setId(id);
+			return "update_recipe";
+		}
+		if(!file.isEmpty())
+		{
+			picService.deletePicture(receta.getFoto());
+			UUID idPic = UUID.randomUUID();
+			picService.uploadPicture(file, idPic);
+			receta.setFoto(idPic);
+		}
+		repo.save(receta);
+		return "redirect:/recetas/list";
+	}
+	
+	@PreAuthorize("hasAuthority('admin')")
+	@GetMapping("/delete/{id}")
+	public String deleteRecipe(@PathVariable("id") Long id, Model model)
+	{
+		Receta receta= repo.findById(id).orElseThrow(() ->new IllegalAnnotationsException("Invalid recipe Id:"+id));
+		repo.delete(receta);
+		picService.deletePicture(receta.getFoto());
+		model.addAttribute("recipes",repo.findAll());
+		return "list_recipes";
+	}
+	}
+		
+	
+	
 }
